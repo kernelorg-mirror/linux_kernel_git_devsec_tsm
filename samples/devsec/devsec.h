79b7f974b03887905fcefdfb678fe097b04086b8
Dan Williams <dan.j.williams@intel.com>
2025-02-28 21:52:36 -0800

    samples/devsec: Introduce a PCI device-security bus + endpoint sample
    Establish just enough emulated PCI infrastructure to register a sample
    TSM (platform security manager) driver and have it discover an IDE + TEE
    (link encryption + device-interface security protocol (TDISP)) capable
    device.
    
    Use the existing a CONFIG_PCI_BRIDGE_EMUL to emulate an IDE capable root
    port, and open code the emulation of an endpoint device via simulated
    configuration cycle responses.
    
    The devsec_tsm driver responds to the PCI core TSM operations as if it
    successfully exercised the given interface security protocol message.
    
    The devsec_bus and devsec_tsm drivers can be loaded in either order to
    reflect cases like SEV-TIO where the TSM is PCI-device firmware, and
    cases like TDX Connect where the TSM is a software agent running on the
    host CPU.
    
    Follow-on patches add common code for TSM managed IDE establishment. For
    now, just successfully complete setup and teardown of the DSM (device
    security manager) context as a building block for management of TDI
    (trusted device interface) instances.
    
     # modprobe devsec_bus
        devsec_bus devsec_bus: PCI host bridge to bus 10000:00
        pci_bus 10000:00: root bus resource [bus 00-01]
        pci_bus 10000:00: root bus resource [mem 0xf000000000-0xffffffffff 64bit]
        pci 10000:00:00.0: [8086:7075] type 01 class 0x060400 PCIe Root Port
        pci 10000:00:00.0: PCI bridge to [bus 00]
        pci 10000:00:00.0:   bridge window [io  0x0000-0x0fff]
        pci 10000:00:00.0:   bridge window [mem 0x00000000-0x000fffff]
        pci 10000:00:00.0:   bridge window [mem 0x00000000-0x000fffff 64bit pref]
        pci 10000:00:00.0: bridge configuration invalid ([bus 00-00]), reconfiguring
        pci 10000:01:00.0: [8086:ffff] type 00 class 0x000000 PCIe Endpoint
        pci 10000:01:00.0: BAR 0 [mem 0xf000000000-0xf0001fffff 64bit pref]
        pci_doe_abort: pci 10000:01:00.0: DOE: [100] Issuing Abort
        pci_doe_cache_protocols: pci 10000:01:00.0: DOE: [100] Found protocol 0 vid: 1 prot: 1
        pci 10000:01:00.0: disabling ASPM on pre-1.1 PCIe device.  You can enable it with 'pcie_aspm=force'
        pci 10000:00:00.0: PCI bridge to [bus 01]
        pci_bus 10000:01: busn_res: [bus 01] end is updated to 01
     # modprobe devsec_tsm
        devsec_tsm_pci_probe: pci 10000:01:00.0: devsec: tsm enabled
        __pci_tsm_init: pci 10000:01:00.0: TSM: Device security capabilities detected ( ide tee ), TSM attach
    
    Cc: Bjorn Helgaas <bhelgaas@google.com>
    Cc: Lukas Wunner <lukas@wunner.de>
    Cc: Samuel Ortiz <sameo@rivosinc.com>
    Cc: Alexey Kardashevskiy <aik@amd.com>
    Cc: Xu Yilun <yilun.xu@linux.intel.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

---
+++ samples/devsec/devsec.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+// Copyright(c) 2024 Intel Corporation. All rights reserved.
+
+#ifndef __DEVSEC_H__
+#define __DEVSEC_H__
+extern struct pci_sysdata *devsec_sysdata;
+#endif /* __DEVSEC_H__ */
