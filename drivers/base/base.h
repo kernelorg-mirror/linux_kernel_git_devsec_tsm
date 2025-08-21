0c2585baa3bffe5983b70202882572f4b2610e44
Dan Williams <dan.j.williams@intel.com>
2025-08-20 21:05:25 -0700

    device core: Introduce confidential device acceptance
    Of the many problems to solve with PCIe Trusted Execution Environment
    Device Interface Security Protocol (TDISP) support, this is among the most
    fraught. New device core infrastructure demands a high degree of scrutiny
    especially when touching the long standing kernel policy that the kernel
    trusts devices by default. Previous adjacent proposals in this space (e.g.
    device filter, no bounce buffer flag...) have not moved forward.
    
    So, what is this new 'struct device_private' mechanism, how is it different
    from previous attempts, and why not a bus-device-type specific mechanism
    (e.g.  pci_dev::untrusted, usb_device::authorized, tb_switch::authorized,
    etc...)?
    
    TEE acceptance is not a state that random modules should be allowed to
    change in the common case. A device entering the accepted state is a
    violent operation. Pre-existing MMIO and DMA mappings can not survive this
    event. The device_cc_accept() and device_cc_reject() helpers (where "cc" ==
    "confidential computing") coordinate with driver attachment and are only
    meant for core-kernel bus drivers like the PCI core.
    
    Driver interactions with the "accepted" state are similar to driver
    interactions with the driver-core probe deferral mechanism (also managed in
    'struct device_private'). TEE I/O aware drivers are responsible for
    preparing the device for acceptance and then waiting for the accept event.
    That maps cleanly to the probe deferral mechanism and device_cc_probe()
    helps coordinates that handoff.
    
    When the device enters the TEE, other subsystems need to behave
    differently. For example, the IOMMU/DMA mapping subsystem needs to switch
    DMA mapping requests from SWIOTLB bounce buffering to direct-DMA to private
    memory. That device state is communicated via device_cc_accepted() in a
    common way.
    
    The observation is that PCI is not the only bus that has designs on
    interacting with a TEE acceptance state. The "adjacent proposals" mentioned
    before include platform firmware and embedded buses that want to accept
    devices into the TEE. A bus-type-specific flag would be an ongoing
    maintenance burden for each new bus that adds TEE acceptance support.
    
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Jason Gunthorpe <jgg@ziepe.ca>
    Cc: Marek Szyprowski <m.szyprowski@samsung.com>
    Cc: Robin Murphy <robin.murphy@arm.com>
    Cc: Roman Kisel <romank@linux.microsoft.com>
    Cc: Bjorn Helgaas <bhelgaas@google.com>
    Cc: Samuel Ortiz <sameo@rivosinc.com>
    Cc: Alexey Kardashevskiy <aik@amd.com>
    Cc: Xu Yilun <yilun.xu@linux.intel.com>
    Cc: "Aneesh Kumar K.V" <aneesh.kumar@kernel.org>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: "Rafael J. Wysocki" <rafael@kernel.org>
    Cc: Danilo Krummrich <dakr@kernel.org>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/base/base.h
+++ drivers/base/base.h
@@ -99,6 +99,8 @@ struct driver_private {
  *		    resources needed by the device; typically because it depends
  *		    on another driver getting probed first.
  * @async_driver: pointer to device driver awaiting probe via async_probe
+ * @cc_accepted: track the TEE acceptance state of the device for deferred
+ *		 probing, MMIO mapping type, and SWIOTLB bypass for private memory DMA.
  * @deferred_probe_reason: capture the -EPROBE_DEFER message emitted with
  *			   dev_err_probe() for later retrieval via debugfs
  * @device: pointer back to the struct device that this structure is
@@ -118,6 +120,9 @@ struct device_private {
 	struct list_head deferred_probe;
 	const struct device_driver *async_driver;
 	char *deferred_probe_reason;
+#ifdef CONFIG_CONFIDENTIAL_DEVICES
+	bool cc_accepted;
+#endif
 	struct device *device;
 	u8 dead:1;
 };
