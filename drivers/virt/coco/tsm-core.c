722f1b0155cfba90fd41d0bf884b0b5752c55fec
Dan Williams <dan.j.williams@intel.com>
2025-10-15 16:22:51 -0700

    PCI/TSM: Establish Secure Sessions and Link Encryption
    The PCIe 7.0 specification, section 11, defines the Trusted Execution
    Environment (TEE) Device Interface Security Protocol (TDISP).  This
    protocol definition builds upon Component Measurement and Authentication
    (CMA), and link Integrity and Data Encryption (IDE). It adds support for
    assigning devices (PCI physical or virtual function) to a confidential VM
    such that the assigned device is enabled to access guest private memory
    protected by technologies like Intel TDX, AMD SEV-SNP, RISCV COVE, or ARM
    CCA.
    
    The "TSM" (TEE Security Manager) is a concept in the TDISP specification
    of an agent that mediates between a "DSM" (Device Security Manager) and
    system software in both a VMM and a confidential VM. A VMM uses TSM ABIs
    to setup link security and assign devices. A confidential VM uses TSM
    ABIs to transition an assigned device into the TDISP "RUN" state and
    validate its configuration. From a Linux perspective the TSM abstracts
    many of the details of TDISP, IDE, and CMA. Some of those details leak
    through at times, but for the most part TDISP is an internal
    implementation detail of the TSM.
    
    CONFIG_PCI_TSM adds an "authenticated" attribute and "tsm/" subdirectory
    to pci-sysfs. Consider that the TSM driver may itself be a PCI driver.
    Userspace can watch for the arrival of a "TSM" device,
    /sys/class/tsm/tsm0/uevent KOBJ_CHANGE, to know when the PCI core has
    initialized TSM services.
    
    The operations that can be executed against a PCI device are split into
    two mutually exclusive operation sets, "Link" and "Security" (struct
    pci_tsm_{link,security}_ops). The "Link" operations manage physical link
    security properties and communication with the device's Device Security
    Manager firmware. These are the host side operations in TDISP. The
    "Security" operations coordinate the security state of the assigned
    virtual device (TDI). These are the guest side operations in TDISP.
    
    Only "link", Secure Session and physical Link Encryption, operations are
    defined at this stage with placeholders for the device security, Trusted
    Computing Base entry / exit, operations.
    
    The locking allows for multiple devices to be executing commands
    simultaneously, one outstanding command per-device and an rwsem
    synchronizes the implementation relative to TSM registration/unregistration
    events.
    
    Thanks to Wu Hao for his work on an early draft of this support.
    
    Cc: Lukas Wunner <lukas@wunner.de>
    Cc: Samuel Ortiz <sameo@rivosinc.com>
    Cc: Alexey Kardashevskiy <aik@amd.com>
    Acked-by: Bjorn Helgaas <bhelgaas@google.com>
    Co-developed-by: Xu Yilun <yilun.xu@linux.intel.com>
    Signed-off-by: Xu Yilun <yilun.xu@linux.intel.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/virt/coco/tsm-core.c
+++ drivers/virt/coco/tsm-core.c
@@ -9,15 +9,18 @@
 #include <linux/device.h>
 #include <linux/module.h>
 #include <linux/cleanup.h>
+#include <linux/pci-tsm.h>
 
 static struct class *tsm_class;
 static DECLARE_RWSEM(tsm_rwsem);
 static DEFINE_IDR(tsm_idr);
 
-struct tsm_dev {
-	struct device dev;
-	int id;
-};
+/* Caller responsible for ensuring it does not race tsm_dev unregistration */
+struct tsm_dev *find_tsm_dev(int id)
+{
+	guard(rcu)();
+	return idr_find(&tsm_idr, id);
+}
 
 static struct tsm_dev *alloc_tsm_dev(struct device *parent)
 {
@@ -42,6 +45,28 @@ static struct tsm_dev *alloc_tsm_dev(struct device *parent)
 	return no_free_ptr(tsm_dev);
 }
 
+static struct tsm_dev *tsm_register_pci_or_reset(struct tsm_dev *tsm_dev,
+						 struct pci_tsm_ops *pci_ops)
+{
+	int rc;
+
+	if (!pci_ops)
+		return tsm_dev;
+
+	tsm_dev->pci_ops = pci_ops;
+	rc = pci_tsm_register(tsm_dev);
+	if (rc) {
+		dev_err(tsm_dev->dev.parent,
+			"PCI/TSM registration failure: %d\n", rc);
+		device_unregister(&tsm_dev->dev);
+		return ERR_PTR(rc);
+	}
+
+	/* Notify TSM userspace that PCI/TSM operations are now possible */
+	kobject_uevent(&tsm_dev->dev.kobj, KOBJ_CHANGE);
+	return tsm_dev;
+}
+
 static void put_tsm_dev(struct tsm_dev *tsm_dev)
 {
 	if (!IS_ERR_OR_NULL(tsm_dev))
@@ -51,7 +76,7 @@ static void put_tsm_dev(struct tsm_dev *tsm_dev)
 DEFINE_FREE(put_tsm_dev, struct tsm_dev *,
 	    if (!IS_ERR_OR_NULL(_T)) put_tsm_dev(_T))
 
-struct tsm_dev *tsm_register(struct device *parent)
+struct tsm_dev *tsm_register(struct device *parent, struct pci_tsm_ops *pci_ops)
 {
 	struct tsm_dev *tsm_dev __free(put_tsm_dev) = alloc_tsm_dev(parent);
 	struct device *dev;
@@ -69,12 +94,14 @@ struct tsm_dev *tsm_register(struct device *parent)
 	if (rc)
 		return ERR_PTR(rc);
 
-	return no_free_ptr(tsm_dev);
+	return tsm_register_pci_or_reset(no_free_ptr(tsm_dev), pci_ops);
 }
 EXPORT_SYMBOL_GPL(tsm_register);
 
 void tsm_unregister(struct tsm_dev *tsm_dev)
 {
+	if (tsm_dev->pci_ops)
+		pci_tsm_unregister(tsm_dev);
 	device_unregister(&tsm_dev->dev);
 }
 EXPORT_SYMBOL_GPL(tsm_unregister);
